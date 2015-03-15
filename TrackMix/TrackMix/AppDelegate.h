3bd38663e58a63ca90d0625684ab9892866d7ddc
cokerxi <155154078@qq.com>
2015-03-15 12:19:42 +0800

    with xib

---
+++ TrackMix/TrackMix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrackMix
+//
+//  Created by air on 15/3/15.
+//  Copyright (c) 2015年 air. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TrackMix/TrackMix/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TrackMix
+//
+//  Created by air on 15/3/15.
+//  Copyright (c) 2015年 air. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
